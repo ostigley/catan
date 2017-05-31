0b2283ad8877c5c233abc9c569e6eecda9320d7c
Oliver Stigley <oliverstigley@gmail.com>
2017-05-31 21:30:38 +1200

    clean firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,5 @@
 import firebase from 'firebase'
-
+import { config } from './config.js'
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyB_PSrC5ucNuwiqCsudHZSn1mqNF6Ms-as",
-  authDomain: "catan-484ab.firebaseapp.com",
-  databaseURL: "https://catan-484ab.firebaseio.com",
-  projectId: "catan-484ab",
-  storageBucket: "catan-484ab.appspot.com",
-  messagingSenderId: "853628889150"
-};
 
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
